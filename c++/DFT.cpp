a374b001b5de9c195f9f61eacf4956ac897d080e
Dina Nashaat <dina.nashaat@gmail.com>
2016-08-09 01:15:16 +0200

    Change DFT input/output from Complex to Double

--- c++/DFT.cpp
+++ c++/DFT.cpp
@@ -4,7 +4,7 @@
 #include<complex>
 using namespace std;
 
-void computeDFT (vector<complex<double>> &in, vector<complex<double>> &out)
+void computeDFT (vector<double> &in, vector<double> &outReal, vector<double> &outImag)
 {
     int n = in.size();
     double sumreal, sumimag;
@@ -16,25 +16,25 @@ void computeDFT (vector<complex<double>> &in, vector<complex<double>> &out)
         for (int k=0;k<n;k++)
         {
             angle = (2* M_PI *k*i)/n;
-            sumreal += real(in[k])*cos(angle) + imag(in[k])*sin(angle);
-            sumimag += -real(in[k])*sin(angle) + imag(in[k])*cos(angle);
+            sumreal += in[k]*cos(angle);
+            sumimag += -in[k]*sin(angle);
         }
-        out[i].real(sumreal);
-        out[i].imag(sumimag);
+        outReal[i] = sumreal;
+        outImag[i] = sumimag;
     }
 }
 
-void printDFT(vector<complex<double>> &in)
+void printDFT(vector<double> &inReal, vector<double> &inImag)
 {
-    for(int i=0;i<in.capacity();i++)
+    for(int i=0;i<inReal.capacity();i++)
     {
-        cout << real(in[i]);
+        cout << inReal[i];
         cout << " ";
-        if (imag(in[i]) >= 0)
+        if (inImag[i] >= 0)
             cout << "+";
         else
             cout << "-";
         cout << " ";
-        cout << abs(imag(in[i])) << "i" <<endl;
+        cout << abs(inImag[i]) << "i" <<endl;
     }
 }
