8307f676f251e68f9da8b27e6d867f5f0bcd18e1
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2016-08-09 00:14:30 +0200

    Build main.cpp

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,29 +1,46 @@
 #include <iostream>
+#include <string.h>
 #include "DFT.cpp"
 #include "DCT.cpp"
 #include "inputReader.cpp"
+
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::vector<complex<double>> matrix = {4.0,3.0,2.0};
-    std::vector<complex<double>> out(matrix.capacity());
-    std::vector<double> matrixDCT = {4.0,3.0,2.0};
-    std::vector<double> outDCT(matrixDCT.capacity());
-    computeDFT(matrix,out);
-    cout << "DFT: " <<endl;
-    printDFT(out);
-    cout << "DCT: " <<endl;
-    computeDCT(matrixDCT,outDCT);
-    printDCT(outDCT);
+    //argv[0] filename used by system to compile
+    //argv[1] the filename for the dataset to read
+    //argv[2] the compression function to be used
+    //argv[3] the index of the column to compress
+
+    /*if( argc != 2 ){
+        printf("usage: %s filename", argv[0]);
+        return 0;
+    }*/
 
     std::vector<vector<double>> inputMatrix;
-    readInput("Sample.csv",inputMatrix);
-    printMat(inputMatrix);
 
-    std::vector<vector<double>> reversedMatrix;
-    reversedMatrix = transMatrix(inputMatrix);
-    printMat(reversedMatrix);
+    try{
+        readInput(argv[1], inputMatrix);
+    }catch(exception e){
+        printf("The file you are trying to read does not exists");
+        return 0;
+    }
+
+    std::vector<double> outputMatrix(inputMatrix.capacity());
+
+    // strcmp returns 0 if the strings are equal
+    if(!strcmp(argv[2], "dft")){
+        cout << "DFT: " << endl;
+        computeDFT(inputMatrix[0], outputMatrix);
+        printDFT(outputMatrix);
+    }else if(!strcmp(argv[2], "dct")){
+        cout << "DCT: " <<endl;
+        computeDCT(inputMatrix[0], outputMatrix);
+        printDCT(outputMatrix);
+    }else{
+        printf("Compression function is not correct");
+    }
 
     return 0;
 }
